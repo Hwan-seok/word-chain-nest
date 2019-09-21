6276cfb27e304b2c67cba900182211c2fe50aeac
Hwan-seok <tttkhs@khu.ac.kr>
2019-09-22 05:32:25 +0900

    - JWT service 구현   - Sign   - verify

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Post, Param, Body } from '@nestjs/common';
+import { JWTService } from './jwt/jwt.service';
+import { UserDTO } from '../user/dto/user.dto';
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly jwtService: JWTService) {}
+
+  @Post()
+  async login(@Body() userDTO: UserDTO) {
+    return this.jwtService.generate(userDTO);
+  }
+  @Get(':token')
+  async wer(@Param('token') token) {
+    return this.jwtService.decrypt(token);
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JWTService } from './jwt/jwt.service';
+import { UserModule } from '../user/user.module';
+import { ConfigModule } from '../config/config.module';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [UserModule],
+  providers: [AuthService, JWTService],
+  controllers: [AuthController],
+  exports: [JWTService],
 })
 export class AuthModule {}
