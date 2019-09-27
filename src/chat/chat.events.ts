1683a1f0d91341db4d52ad4883eea2d462f961e6
Hwan-seok <tttkhs@khu.ac.kr>
2019-09-28 05:14:21 +0900

    - #9 돌려서 해결 - 클라이언트와 이벤트 기반 통신 테스트 완료   - to, broadcast, ...

--- src/chat/chat.events.ts
+++ src/chat/chat.events.ts
@@ -3,23 +3,61 @@ import {
   WebSocketGateway,
   WebSocketServer,
   WsResponse,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Client, Server } from 'socket.io';
+import { Client, Server, Socket } from 'socket.io';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { JWTService } from '../auth/jwt/jwt.service';
+import { UserEntity } from '../user/user.entity';
+import { RoomService } from '../room/room.service';
 
-@WebSocketGateway()
-export class ChatEvents {
+@WebSocketGateway({ namespace: '/line' })
+export class ChatEvents implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(
+    private readonly jwtService: JWTService,
+    private readonly roomService: RoomService,
+  ) {}
+
   @WebSocketServer()
   server: Server;
 
+  async handleConnection(socket) {
+    const { roomNum, userId } = await this.throughConnection(socket);
+
+    this.roomService.joinUser(roomNum, userId);
+    socket.join(roomNum);
+    this.server.to(roomNum).emit('joinUser', userId);
+  }
+  async handleDisconnect(socket) {
+    const { roomNum, userId } = await this.throughConnection(socket);
+    
+    this.roomService.leaveUser(roomNum, userId);
+    socket.leave(roomNum);
+    this.server.to(roomNum).emit('leaveUser', userId);
+  }
+
   @SubscribeMessage('events')
-  findAll(client: Client, data: any): Observable<WsResponse<number>> {
-    return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+  findAll(socket: Socket, data: any) {
+    socket.broadcast.to('1').emit('events', 'asdf');
   }
 
   @SubscribeMessage('identity')
   async identity(client: Client, data: number): Promise<number> {
     return data;
+  }
+
+  async throughConnection(socket): Promise<any> {
+    const param = socket.handshake.query;
+    const accessToken: string = param.token;
+    const roomNum: string = param.roomNum;
+
+    const user: UserEntity = await this.jwtService.verify(accessToken, true);
+    const userId = user.id;
+
+    return { roomNum, userId };
   }
 }
